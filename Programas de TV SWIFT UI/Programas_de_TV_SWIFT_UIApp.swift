140e49b437851e789e43300088a4ef6f1d6e2b9a
Pablo Zaved <pablo.azevedo@sou.inteli.edu.br>
2026-05-25 10:46:39 -0300

    adiciona x code

---
+++ Programas de TV SWIFT UI/Programas_de_TV_SWIFT_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programas_de_TV_SWIFT_UIApp.swift
+//  Programas de TV SWIFT UI
+//
+//  Created by Pablo Azevedo on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Programas_de_TV_SWIFT_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
